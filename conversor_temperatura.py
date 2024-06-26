61630b24a36ecba63f9a4c58c701d410d83ab30b
Carina Camargo <catcatarina193@gmail.com>
2024-06-26 16:18:17 -0300

    Implements Celsius para fahrenheit converter

--- conversor_temperatura.py
+++ conversor_temperatura.py
@@ -2,8 +2,12 @@ class ConversorDeTemperatura:
     def __init__():
         pass
 
-    def celsius_para_fahrenheit():
-        pass
+    def celsius_para_fahrenheit(valor_celsius):
+        return valor_celsius * (9/5) + 32
 
     def fahrenheit_para_celsius():
         pass
+
+#Area de Teste
+result = ConversorDeTemperatura.celsius_para_fahrenheit(0)
+print('result', result)
