e09069aa78ef01a47b30b83feb88552bb42ee394
Carina Camargo <catcatarina193@gmail.com>
2024-06-26 15:59:59 -0300

    Implements Centimetros para Metros converter

--- conversor_medidas.py
+++ conversor_medidas.py
@@ -2,8 +2,12 @@ class ConversorDeMedidas:
     def __init__():
         pass
 
-    def centimetros_para_metros():
-        pass
+    def centimetros_para_metros(valor_cm):
+        return valor_cm /100
 
     def metros_para_centimetros():
         pass
+
+#Area de Teste
+result = ConversorDeMedidas.centimetros_para_metros(100)
+print('result:', result)
