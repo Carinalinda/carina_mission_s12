9ea50b32b4ef258013f798d8195c6982599238c9
Carina Camargo <catcatarina193@gmail.com>
2024-07-03 17:47:37 -0300

    Implement Conversor de  temperatura menu

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from conversor_medidas import ConversorDeMedidas
+from conversor_temperatura import ConversorDeTemperatura
 
 print('''
 [1]. Centimetros para Metros
 [2]. Metros para Centimetros 
+[3]. Celsius para fahrenheit
+[4]. Fahrenheit para celsius
 ''')
 
 escolha = input('Escolha uma opção: ')
@@ -12,5 +15,8 @@ if escolha == '1':
     result =  ConversorDeMedidas.centimetros_para_metros(value)
 elif escolha == '2':
     result = ConversorDeMedidas.metros_para_centimetros(value)
-    
+elif escolha == '3':
+    result = ConversorDeTemperatura.celsius_para_fahrenheit(value)
+else:
+    result = ConversorDeTemperatura.fahrenheit_para_celsius(value)
 print('Result:',result)
