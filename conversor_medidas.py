21aa0285d35d05ba17c7ac46569e3bdffc21e99d
Carina Camargo <catcatarina193@gmail.com>
2024-07-03 17:26:00 -0300

    Removi area de teste

--- conversor_medidas.py
+++ conversor_medidas.py
@@ -7,7 +7,3 @@ class ConversorDeMedidas:
 
     def metros_para_centimetros(valor_metros):
         return valor_metros * 100
-
-#Area de Teste
-result = ConversorDeMedidas.metros_para_centimetros(1)
-print('result:', result)
