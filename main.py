99ee92dfc2d58951c9d7ade1e199d5e8ab1dee5d
Carina Camargo <catcatarina193@gmail.com>
2024-07-03 17:38:03 -0300

    Implements Metros para Centimetros manu converter

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from conversor_medidas import ConversorDeMedidas
 
 print('''
 [1]. Centimetros para Metros
+[2]. Metros para Centimetros 
 ''')
 
 escolha = input('Escolha uma opção: ')
@@ -9,5 +10,7 @@ value = float( input('Digite um valor: '))
 
 if escolha == '1':
     result =  ConversorDeMedidas.centimetros_para_metros(value)
-
+elif escolha == '2':
+    result = ConversorDeMedidas.metros_para_centimetros(value)
+    
 print('Result:',result)
